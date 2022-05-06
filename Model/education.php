934afca918b8d0735c0931fbbaa907e7047eb9db
alban-okoby <albanokoby225@gmail.com>
2022-05-06 19:32:16 +0200

    Update education.php

--- Model/education.php
+++ Model/education.php
@@ -1,7 +1,7 @@
 <?php 
     require 'database.php'; 
 
-    $sqlEducation = 'SELECT * FROM education ORDER BY id';
+    $sqlEducation = 'SELECT * FROM education ORDER BY id ASC LIMIT 6';
     $educationStatement = $connect->query($sqlEducation);
     $educationStatement->execute();
     $rows = $educationStatement->fetchAll();
