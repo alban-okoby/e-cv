73990406a6c8e32c6bed1cd69462ee5a45dbb2dd
alban-okoby <albanokoby225@gmail.com>
2022-06-05 01:23:48 +0200

    Begining insert statement to About

--- Controller/AboutController.php
+++ Controller/AboutController.php
@@ -1,7 +1,27 @@
 <?php 
     require 'database.php'; 
 
+    // View data
     $sqlAbout = 'SELECT * FROM about';
     $aboutStatement = $connect->prepare($sqlAbout);
     $aboutStatement->execute();
     $row = $aboutStatement->fetchAll();
+
+    // Insert statement
+    $aboutStatement = $connect->prepare("INSERT INTO about (title, name, age, qualification, post, langue, button, annee_exp, projet_termine, clt_satisfait, formations_organisees, certification) 
+    VALUES (:title, :name, :age, :qualification, :post, :langue, :button, :annee_exp, :projet_termine, :clt_satisfait, :formations_organisees, :certification)");
+    $aboutStatement->execute(array(
+                                    ':title' => $title,
+                                    ':name' => $name,
+                                    ':age' => $age,
+                                    ':qualification' => $qualification,
+                                    ':post' => $post,
+                                    ':langue' => $langue,
+                                    ':button' =>button,
+                                    ':annee_exp' => $annee_exp,
+                                    ':projet_termine' => $projet_termine,
+                                    ':clt_satisfait' => $clt_satisfait,
+                                    ':formations_organisees' => $formations_organisees,
+                                    ':certification' => $certification
+    ));
+
