0022e5fcbe104c802eaac70a443bad71c628a856
alban-okoby <albanokoby225@gmail.com>
2022-06-05 17:56:12 +0200

    Ajout de la section éducation admin

--- admin/index.php
+++ admin/index.php
@@ -140,6 +140,43 @@
                     }
                 ?>        
         </div>
+        <div class="home">
+                <h2>Education / Experiences </h2>
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Années</th>
+                            <th>Post(s) ou Niveau d'étude</th>
+                            <th>Description</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $connexion = DataBase::connect();
+                        require_once('../src/Controller/EducationController.php');
+                        foreach ($rows as $education) {
+                            echo
+                            '<tr>';
+                            echo '<td>'.$education['annee'].'</td>';
+                            echo '<td>'.$education['post'].'</td>';
+                            echo '<td>'.$education['description'].'</td>';
+                            echo '<td width="300">';
+                            echo '<a class="btn btn-default" href="view.php?id='.$education['id'].'" ><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
+                            echo "  ";
+                            echo '<a class="btn btn-primary" href="update.php?id='.$education['id'].'" ><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
+                            echo "  ";
+                            echo '<a class="btn btn-danger" href="delete.php?id='.$education['id'].'" ><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
+                            echo '</td>';
+                            echo '</tr>';
+
+                        }
+                        DataBase::disconnect();
+                        ?>
+
+                    </tbody>
+                </table>
+            </div>
     </div>
 </body>
 </html>
