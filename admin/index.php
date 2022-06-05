b84dcd1573c0f84000abe7ddb135092bc8b4c766
alban-okoby <albanokoby225@gmail.com>
2022-06-05 12:23:16 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -12,46 +12,131 @@
 	<div class="container container-admin">
 		<div class="row">
 			<h1><strong>Votre espace admin </strong><a href="inserttest.php?id=1" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-plus"></span>Ajouter</a> <a href="../../index.php" class="btn btn-warning btn-lg"></span>Aller à l'acceuil</a></h1>
-			<table class="table table-striped table-bordered">
-				<thead>
-					<tr>
-						<th>Salutation</th>
-						<th>Nom & Prenom(s)</th>
-						<th>Emploi</th>
-						<th>Citation</th>
-						<th>Button</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					require '../src/Controller/database.php';
-					$connexion = DataBase::connect();
-                    require_once('../src/Controller/homeController.php');
-					foreach ($row as $home) {
-						echo
-						'<tr>';
-						echo '<td>'.$home['title'].'</td>';
-						echo '<td>'.$home['name'].'</td>';
-						echo '<td>'.$home['post'].'</td>';
-						echo '<td>'.$home['description'].'</td>';
-						echo '<td>'.$home['button'].'</td>';
-						echo '<td width="300">';
-						echo '<a class="btn btn-default" href="view.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
-						echo "  ";
-						echo '<a class="btn btn-primary" href="update.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
-						echo "  ";
-						echo '<a class="btn btn-danger" href="delete.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
-						echo '</td>';
-						echo '</tr>';
+            <div class="home">
+                <h2>Section Home</h2>
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Salutation</th>
+                            <th>Nom & Prenom(s)</th>
+                            <th>Emploi</th>
+                            <th>Citation</th>
+                            <th>Button</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        require '../src/Controller/database.php';
+                        $connexion = DataBase::connect();
+                        require_once('../src/Controller/homeController.php');
+                        foreach ($row as $home) {
+                            echo
+                            '<tr>';
+                            echo '<td>'.$home['title'].'</td>';
+                            echo '<td>'.$home['name'].'</td>';
+                            echo '<td>'.$home['post'].'</td>';
+                            echo '<td>'.$home['description'].'</td>';
+                            echo '<td>'.$home['button'].'</td>';
+                            echo '<td width="300">';
+                            echo '<a class="btn btn-default" href="view.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
+                            echo "  ";
+                            echo '<a class="btn btn-primary" href="update.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
+                            echo "  ";
+                            echo '<a class="btn btn-danger" href="delete.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
+                            echo '</td>';
+                            echo '</tr>';
 
-					}
-					DataBase::disconnect();
-					?>
+                        }
+                        DataBase::disconnect();
+                        ?>
 
-				</tbody>
-			</table>
-		</div>
-	</div>
+                    </tbody>
+                </table>
+            </div>
+            <div class="about">
+                <h2>Section About</h2>
+                <?php
+                        $connexion = DataBase::connect();
+                        require_once('../src/Controller/AboutController.php');
+                        foreach ($row as $about) {
+                    ?>
+                <table class="table table-striped table-bordered">
+                    <div class="first">
+
+                        <thead>
+                            <tr>
+                                <th>Titre</th>
+                                <th>Nom & Prenom(s)</th>
+                                <th>Age</th>
+                                <th>Qualification</th>
+                                <th>Emploi(s)</th>
+                                <th>Langue(s)</th>
+                                <th>Télécharger</th>
+                                <th>Actions</th>
+                        </tr>
+				    </thead>
+                    <tbody>
+                        <?php
+                            echo
+                            '<tr>';
+                            echo '<td>'.$about['title'].'</td>';
+                            echo '<td>'.$about['name'].'</td>';
+                            echo '<td>'.$about['age'].'</td>';
+                            echo '<td>'.$about['qualification'].'</td>';
+                            echo '<td>'.$about['post'].'</td>';
+                            echo '<td>'.$about['langue'].'</td>';
+                            echo '<td>'.$about['button'].'</td>';
+                            echo '<td width="300">';
+                            echo '<a class="btn btn-default" href="view.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
+                            echo "  ";
+                            echo '<a class="btn btn-primary" href="update.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
+                            echo "  ";
+                            echo '<a class="btn btn-danger" href="delete.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
+                            echo '</td>';
+                            echo '</tr>';
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="second">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Annee_exp</th>
+                                <th>Projet Achevé(s)</th>
+                                <th>Client(s) Satisfait(s)</th>
+                                <th>Formations Dispensées</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    echo
+                                    '<tr>';
+                                    echo '<td>'.$home['annee_exp'].'</td>';
+                                    echo '<td>'.$home['projet_termine'].'</td>';
+                                    echo '<td>'.$home['clt_satisfait'].'</td>';
+                                    echo '<td>'.$home['formations_organisees'].'</td>';
+                                    echo '<td>'.$home['certification'].'</td>';
+                                    echo '<td width="300">';
+                                    echo '<a class="btn btn-default" href="view.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
+                                    echo "  ";
+                                    echo '<a class="btn btn-primary" href="update.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
+                                    echo "  ";
+                                    echo '<a class="btn btn-danger" href="delete.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
+                                    echo '</td>';
+                                    echo '</tr>';
+    
+                                ?>
+    
+                            </tbody>
+                    </table>
+                </div>
+                <?php
+                    }
+                ?>        
+        </div>
+    </div>
 </body>
 </html>
