3c1e9ac8c86df84c437e116cc596bc9ddeca58b7
alban-okoby <albanokoby225@gmail.com>
2022-06-05 04:37:50 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -11,37 +11,37 @@
 	<div class="col-md-offset-2"></div>	
 	<div class="container container-admin">
 		<div class="row">
-			<h1><strong>Liste des éléments </strong><a href="inserttest.php?id=1" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-plus"></span>Ajouter</a> <a href="../../index.php" class="btn btn-warning btn-lg"></span>Aller à l'acceuil</a></h1>
+			<h1><strong>Votre espace admin </strong><a href="inserttest.php?id=1" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-plus"></span>Ajouter</a> <a href="../../index.php" class="btn btn-warning btn-lg"></span>Aller à l'acceuil</a></h1>
 			<table class="table table-striped table-bordered">
 				<thead>
 					<tr>
-						<th>Nom</th>
-						<th>Description</th>
-						<th>Prix (FCFA)</th>
-						<th>Catégorie</th>
-						<th>Action</th>
+						<th>Salutation</th>
+						<th>Nom & Prenom(s)</th>
+						<th>Emploi</th>
+						<th>Citation</th>
+						<th>Button</th>
+						<th>Actions</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php
-					require 'database.php';
-					$db = DataBase::connect();
-					$statement = $db->query('SELECT items.id, items.name, items.description, items.price, categories.name AS 						category
-											 FROM items LEFT JOIN categories ON items.category = categories.id
-											 ORDER by items.id DESC LIMIT 20');
-					while ($item = $statement -> fetch()) {
+					require '../src/Controller/database.php';
+					$connexion = DataBase::connect();
+                    require_once('../src/Controller/homeController.php');
+					foreach ($row as $home) {
 						echo
 						'<tr>';
-						echo '<td>'.$item['name'].'</td>';
-						echo '<td>'.$item['description'].'</td>';
-						echo '<td>'.number_format((float)$item['price'],3,',','').'</td>';
-						echo '<td>'.$item['category'].'</td>';
+						echo '<td>'.$home['title'].'</td>';
+						echo '<td>'.$home['name'].'</td>';
+						echo '<td>'.$home['post'].'</td>';
+						echo '<td>'.$home['description'].'</td>';
+						echo '<td>'.$home['button'].'</td>';
 						echo '<td width="300">';
-						echo '<a class="btn btn-default" href="view.php?id='.$item['id'].'" ><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
+						echo '<a class="btn btn-default" href="view.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-eye-open"></span> Voir</a>';
 						echo "  ";
-						echo '<a class="btn btn-primary" href="update.php?id='.$item['id'].'" ><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
+						echo '<a class="btn btn-primary" href="update.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-pencil"></span> Modifier</a>';
 						echo "  ";
-						echo '<a class="btn btn-danger" href="delete.php?id='.$item['id'].'" ><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
+						echo '<a class="btn btn-danger" href="delete.php?id='.$home['id'].'" ><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
 						echo '</td>';
 						echo '</tr>';
 
