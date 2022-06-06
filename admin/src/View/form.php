0b4fc6d6677397f8b2f2cd2e1dafb91c706d9a9d
alban-okoby <albanokoby225@gmail.com>
2022-06-06 18:46:58 +0200

    Create form.php

---
+++ admin/src/View/form.php
@@ -0,0 +1,31 @@
+<?php 
+    require_once '../Controller/PortfolioController.php'
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-CV | Alban Okoby</title>
+    <link rel="stylesheet" href="../css/Admin.css">
+</head>
+<body>
+    <form class="form" role="form" method="post" enctype="multipart/form-data" action="insert.php">
+        <div class="form-group">
+            <label>Nom du projet :</label>
+            <input class="form-control" type="text" id="name" name="project_name" placeholder="Ex: créa de logo" value="<?php echo $projectName; ?>"> </input> 
+            <span class="help-inline"><?php echo $preojectNameError; ?></span>
+        </div>
+        <div class="form-group">
+            <label for="image">Sélectionner une image:</label>
+            <input type="file" name="image" id="image">
+            <span class="help-inline"><?php echo $projectImageError ;?></span>
+        </div>
+        <div class="form-actions">
+            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span> Ajouter</button>
+            <a href="index.php" class="btn btn-danger"><span class="glyphicon glyphicon-arrow-left"></span> Retour</a>
+        </div>
+    </form>
+</body>
+</html>
