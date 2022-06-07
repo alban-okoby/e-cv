6b80faf04ba1ea7d9dbc2e0a6365fce319afdc28
alban-okoby <albanokoby225@gmail.com>
2022-06-07 08:47:09 +0200

    MVC architecture begining

--- admin/src/Controller/InsertEducation.php
+++ admin/src/Controller/InsertEducation.php
@@ -32,14 +32,7 @@
         }
         if($isUploadSuccess && $isSuccess){
             
-            $connect = DataBase::connect();
-            $educationInsert = $connect->prepare("INSERT INTO education(annee, post, description) VALUES(:annee, :post, :description)");
-            $educationInsert->execute(array(
-                                            'annee' => $annee,
-                                            'post' => $post,
-                                            'description' => $description));
-
-            DataBase::disconnect();
+            require_once('../Model/InsertEducationModel.php');
             header("location:../../index.php");
         }
         
