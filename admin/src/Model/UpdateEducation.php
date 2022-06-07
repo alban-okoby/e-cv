a15c9571260b014de53eb14c05d47813668a8df3
alban-okoby <albanokoby225@gmail.com>
2022-06-07 12:26:27 +0200

    Create UpdateEducation.php

---
+++ admin/src/Model/UpdateEducation.php
@@ -0,0 +1,14 @@
+<?php
+
+$connect = DataBase::connect();
+            $educationUpdate = $connect->prepare("INSERT INTO education(id, annee, post, description) WHERE id VALUES(:id, :annee, :post, :description)");
+            $educationUpdate->execute(array(
+                                            'id' => $id,
+                                            'annee' => $annee,
+                                            'post' => $post,
+                                            'description' => $description
+                                        )
+                                    );
+    
+
+DataBase::disconnect();
