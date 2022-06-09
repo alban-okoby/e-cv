70adf4a9dd00bca60795d5ca33cceb4aa24fc34c
alban-okoby <albanokoby225@gmail.com>
2022-06-09 19:27:16 +0200

    Create DeleteEducation.php

---
+++ admin/src/Controller/Delete/DeleteEducation.php
@@ -0,0 +1,49 @@
+<?php 
+    require_once('../../../data/database.php');
+
+    if(!empty($_GET['id'])){
+
+        $id = cleanInput($_GET['id']);
+    }
+
+    // Model script
+    require_once('../Model/DeleteEducationModel.php');
+    
+    function cleanInput($data) 
+    {
+        $id = htmlspecialchars($data);
+        $id = stripslashes($data);
+        $id = trim($data);
+        return $data;
+    }
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-CV | </title>
+    <?= require_once("../../config/links/CdnCss.php"); ?>
+    
+</head>
+<body>
+
+    <div class="container container-admin">
+        <div class="row">
+            <h1><strong style="font-family:arial;"> Supprimer un élément  </strong><span class="glyphicon glyphicon-cross glyphicon-lg"></span></a></h1>
+            <form class="form" role="form" action="DeleteEducation.php" method="post">
+                <p class="alert alert-warning" style="color:black">Etes-vous sûr de vouloir supprimer cet élément ? <small>Cette action est irréversible !</small></p>
+                <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-danger"><span class=""></span> Oui</button>
+                    <a href="../../../" class="btn btn-default"><span class="glyphicon glyphicon"></span> Non</a>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
