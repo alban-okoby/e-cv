27222a05312af2e7cc1c3d196ca90199b2bd35ee
alban-okoby <albanokoby225@gmail.com>
2022-06-09 18:13:57 +0200

    separate the education view

--- admin/src/View/EachEducationItemView.php
+++ admin/src/View/EachEducationItemView.php
@@ -7,12 +7,8 @@
     }
 
     // Model script
-    $db = DataBase::connect();
-    $statement = $db->prepare('SELECT education.annee, education.post, education.description FROM education WHERE education.id= ?');
-    $statement->execute((array($id)));
-    $education = $statement->fetch();
-    DataBase::disconnect();
-
+    require_once('../Model/ViewEducationModel.php');
+    
     function cleanInput($data) 
     {
         $id = htmlspecialchars($data);
