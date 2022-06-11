b594c0505fde5b722f55482b471c947c16d18b48
alban-okoby <albanokoby225@gmail.com>
2022-06-11 02:49:22 +0200

    Create PortfolioInsertForm.php

---
+++ admin/src/View/PortfolioInsertForm.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-CV | Alban Okoby</title>
+    
+    <!-- Bootstrap cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap cdn -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="../../css/Admin.css">
+</head>
+<body>
+    <div class="container">
+        <div class="container-form">
+
+            <div class="theform col-lg-offset-3 col-lg-6 col-lg-offset-3">
+                
+                <h3 class="text-center mb-4">Ajouter Un nouveau projet </h3>
+                <form class="form" role="form" method="post" action="InsertPortfolioController.php">
+                    <div class="form-group">
+                        <label for="annee">Nom du projet :</label>
+                        <input class="form-control" type="text" id="projectName" name="projectName" placeholder="Ex: Application de boutique" value="<?php echo $projectName; ?>"> </input> 
+                        <span class="help-inline"><?php echo $projectNameError; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="image">Fichier image :</label>
+                        <input type="file" name="project_img" id="image">
+                        <span class="help-inline"><?php echo $projectImageError ;?></span>
+                    </div>
+                   
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-primary"><span class="fa fa-plus"></span> Ajouter</button>
+                        <a href="../../../index.php" class="btn btn-default">Annuler <span class="fa fa-arrow-right"></span> </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
